782802c2995ad7dc9ea7cccb823a4ea58b0fab48
RochaFrancis <franciscorocha.adm@gmail.com>
2022-06-27 22:17:40 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-tjavu8-8hhstt
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-tjavu8-8hhstt)
---
+++ src/app/product-list/product-list.component.ts
@@ -0,0 +1,28 @@
+import { Component } from '@angular/core';
+
+import { products } from '../products';
+
+@Component({
+  selector: 'app-product-list',
+  templateUrl: './product-list.component.html',
+  styleUrls: ['./product-list.component.css']
+})
+export class ProductListComponent {
+
+  products = [...products];
+
+  share() {
+    window.alert('O Produto foi compartilhado!');
+  }
+
+  onNotify() {
+    window.alert('Você será notificado(a) quando o produto estiver disponível');
+  }
+}
+
+
+/*
+Copyright Google LLC. All Rights Reserved.
+Use of this source code is governed by an MIT-style license that
+can be found in the LICENSE file at https://angular.io/license
+*/
